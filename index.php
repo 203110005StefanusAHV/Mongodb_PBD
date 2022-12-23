5d519a76ff47e4a069e5e539fe66878c798876e7
StefanusAHV <stefanusvalentino7@gmail.com>
2022-12-23 11:07:09 +0700

    Mongodb_PBD

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "<h2>Menampilkan file CSV</h2>";
+echo "<p>Stefanus Arut H.V</p>";
+echo '<table border="1">';
+$start_row = 1;
+if (($csv_file = fopen("10000 Sales Records.csv", "r")) !== FALSE) {
+  while (($read_data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
+    $column_count = count($read_data);
+	echo '<tr>';
+    $start_row++;
+    for ($c=0; $c < $column_count; $c++) {
+        echo "<td>".$read_data[$c] . "</td>";
+    }
+	echo '</tr>';
+  }
+  fclose($csv_file);
+}
+echo '</table>';
+?>
